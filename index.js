79be8b9250a5bb03d015a8d023db63304e33c6cf
vikrantdubey <55702092+vikrantdubey@users.noreply.github.com>
2021-06-19 17:02:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// $("button").click(function(){
+//   $(".port").animate({zoom: '450px'});
+// });
+//
+// $("button").
+// click
+// (function(){
+//   $(".img").
+// fadeToggle
+// (
+// 1000
+// );
+// });
+//
+// $("button").click(function(){
+//   $(".pam").toggle();
+// });
